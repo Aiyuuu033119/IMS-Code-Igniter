2e7409a95fc356e397d1d44acc618e6a088f4d6a
unknown <idestura35@gmail.com>
2021-04-03 20:17:11 +0800

    login

--- application/views/login.php
+++ application/views/login.php
@@ -36,13 +36,13 @@
                                 <label style="margin-left: 8px;" for="your_name">
                                   <i class="fas fa-user"></i>
                                 </label>
-                                <input type="text" name="email" id="email" required="_required"placeholder="username"/>
+                                <input type="text" name="email" id="email" required="_required"placeholder="email"/>
                             </div>
                             <div class="form-group">
                                 <label for="your_pass" style="margin-left: 8px;">
                                   <i class="fas fa-lock"></i>
                                 </label>
-                                <input type="password" name="password" id="password" required="_required" placeholder="Password"/>
+                                <input type="password" name="password" id="password" required="_required" placeholder="password"/>
                             </div>
                             
                             <div class="form-group form-button" style="text-align:center">
@@ -52,6 +52,28 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div class="modal fade" id="error-modal" tabindex="-1" role="dialog" aria-labelledby="modal-notification" aria-hidden="true">
+          <div class="modal-dialog modal-danger modal-dialog-centered modal-" role="document">
+            <div class="modal-content bg-gradient-success">
+
+            <div class="modal-body">
+                
+                <div class="py-3 text-center">
+                  <i class="ni ni-fat-remove ni-3x" style="color: #222;"></i>
+                  <h4 class="heading mt-4" style="color: #222;">Error!</h4>
+                  <p style="color: #222;">Please check your credentials!</p>
+                </div>
+                  
+              </div>
+                
+              <div class="modal-footer mt-0">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Ok, Got it</button>
+              </div>
+                
+            </div>
+          </div>
         </div>
 
     </div>
@@ -75,21 +97,17 @@
         var hasError;
 
         if(getEmail==''&&getPass==''){
-          // toastr.error('Please enter your email!');
-          // toastr.error('Please enter your password!');
-          $('.email-error').text('Email*')
-          $('.pass-error').text('Password*')
+          $('#email').attr('placeholder','enter email')
+          $('#password').attr('placeholder','enter password')
 
           hasError = true;
         }
         else if(getEmail==''){
-          $('.email-error').text('Email*')
-          // toastr.error('Please enter your email!');
+          $('#email').attr('placeholder','enter email')
           hasError = true;
         }
         else if(getPass==''){
-          $('.pass-error').text('Password*')
-          // toastr.error('Please enter your password!');
+          $('#password').attr('placeholder','enter password')
           hasError = true;
         }
         else{
@@ -116,7 +134,11 @@
                 window.top.location = "<?php echo base_url()?>app/dashboard";
               }
               else if(json.msg=='error'){
-                $('#modal-notification-danger').modal('show');
+                $('#email').val('')
+                $('#password').val('')
+                $('#email').attr('placeholder','email')
+                $('#password').attr('placeholder','password')
+                $('#error-modal').modal('show');
               }
             }
           });
